4c2613cb4c263fb18b47991da82d983f53f69a2b
Denis Ivanov <visible@jumph4x.net>
2014-02-08 11:18:59 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MikhailZarovny::Application.config.secret_key_base = '43f1cdb4fe15590ca9ea45a59e93a2d8bb2d771a20c98b1ca8b70d32c8e9d01f993eb6ac3e2a4a89934d05bb9c6201fa821c069d11fca37dfc378c02b8092984'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MikhailZarovny::Application.config.session_store :cookie_store, key: '_mikhail_zarovny_session'
