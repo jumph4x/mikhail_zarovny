ce24370dc3ff0ee722b8f4b457478bb5e92ac68b
Denis Ivanov <visible@jumph4x.net>
2014-02-08 16:43:13 -0800

    convert range to array

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
-YEARS = (1980..Time.now.year).reverse
+YEARS = (1980..Time.now.year).to_a.reverse
 SUBJECT_MATTER = ['Portrait', 'Landscape', 'Mythology', 'Nature Morte']
 DISCIPLINES = ['Oil painting', 'Acrylic painting', 'Engraving', 'Ink drawing']
