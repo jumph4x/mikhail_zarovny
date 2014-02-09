d93bf093b79443b051409e8faa654a09bff01167
Denis Ivanov <visible@jumph4x.net>
2014-02-08 19:35:09 -0800

    Price updating via params

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -71,6 +71,6 @@ class Admin::ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:title, :year, :discipline, :subject_matter, :dimensions, :keywords, :description, :upload)
+      params.require(:image).permit(:title, :year, :discipline, :subject_matter, :dimensions, :keywords, :description, :upload, :price)
     end
 end
